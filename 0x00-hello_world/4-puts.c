c12d1767c7c251577c24857e0fe1c12e6c50aec0
vickyrehinat <vickyrehinat@gmail.com>
2022-10-13 11:59:51 -0700

    amazing C

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main-serves as the entery point for the program
+ *
+ * Return: By default returns zero if no errors otherwise return non-zero value.
+ */
+int main(void)
+{
+	puts("\"programming is like a building a multilingual puzzle");
+	return (0);
+}
