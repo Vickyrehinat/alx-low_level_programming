b75e92d649af9d33bcd50f901a628fcc90773d7d
vickyrehinat <vickyrehinat@gmail.com>
2022-10-18 13:14:44 -0700

    task o

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,25 +1,24 @@
 #include "main.h"
-/**
- *  main - print _putchar
- *  is written by vicky
- *  Return: 0
- */
 
+
+/**
+ *  main - print _putchar and a new line
+ *  Return: always 0
+ */
 int main(void)
 {
-_putchar('_');
-_putchar('p');
-_putcher('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-_putchar('\n');
-return (0);
+	_putchar('_');
+	_putchar('p');
+	_putcher('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
 
 }
-
 ~
 ~
 ~
