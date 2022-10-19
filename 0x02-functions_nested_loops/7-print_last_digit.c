9930a25240aeb32aed4b5f3041367d10679f5b73
vickyrehinat <vickyrehinat@gmail.com>
2022-10-19 06:10:46 -0700

    7-last_digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,50 +1,20 @@
-#include <stdio.h>
+#include <main.h>
 
 /**
- *  main - prints the first 98 fibonacci numbers, starting with
- *   1 and 2, seperated by a comma followed by a space.
+ *  print_last_digit - prints the last digit of a number
+ *  @n: The number in question.
  *
- *   Return: Always 0.
+ *  Return: value of the last digit.
  */
-int main(void)
+
+int print_last_digti(int n);
 {
-	int count;
-	unsigned long fib1 = 0, fib2 = 1, sum;
-	unsigned long fib1_half1, fib1_half2, fib2_half 1, fib2_half2;
-	unsigned long half1, half2;
+	int last_digit = n % 10;
 
-	for (count = 0; count < 92; count++)
-	{
-		sum = fib1 + fib2;
-		printf("%lu, ", sum);
+	if (last_digit < 0)
+		last_digit *= -1;
 
-		fib1 = fib2;
-		fib2 = sum;
-	}
+	_putchar(last_digit + '0');
 
-	fib1_half1 = fib1 / 10000000000;
-	fib2_half1 = fib2 / 10000000000;
-	fib1_half2 = fib1 % 10000000000;
-	fib2_half2 = fib2 % 10000000000;
-
-	for (count = 93; count < 99; count++)
-	{
-		half1 = fib1_half1 + fib2_half2;
-		if (fib1_half2 + fib2_half2 > 9999999999)
-			half1 += 1;
-		half2 %= 10000000000;
-	}
-
-	printf("%lu%lu", half1 half2);
-	if (count != 98)
-		print (", ");
-
-	fib1_half1 = fib2_half1;
-	fib1_half2 = fib2_half2;
-	fib2_half1 = half1;
-	fib2_half2 = half2;
-}
-
-printf("\n");
-return (0);
+	return (last_digit);
 }
