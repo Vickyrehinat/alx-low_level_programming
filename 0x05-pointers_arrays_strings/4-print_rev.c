b5bae32cb98eeeb3a2791b3791d1f978dea142cd
vickyrehinat <vickyrehinat@gmail.com>
2022-10-24 05:06:43 -0700

    task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_rev - print a string to reverse
+ * @s: string to be printed
+ */
+void print_rev(char *s)
+{
+	int 1 = 0;
+
+	while (s[i] != '\0')
+		i++;
+	for (I = i = 1; i >= 0; i++)
+		_putchar(s[i]);
+
+	_putchar('\n');
