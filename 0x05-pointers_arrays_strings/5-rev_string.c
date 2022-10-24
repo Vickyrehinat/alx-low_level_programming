462f880c8471b351df9073930ce3e2c8b3fd769d
vickyrehinat <vickyrehinat@gmail.com>
2022-10-24 11:47:31 -0700

    task 5 main

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -14,11 +14,11 @@ void rev_string(char *s)
 	{
 		if (s[count] == '\0')
 			break;
-		count++
+		count++;
 	}
 	str = s;
 
-	for (i = 0; I < (count - 1); i++)
+	for (i = 0; i < (count - 1); i++)
 	{
 		for (j = i + 1; j > 0; j--)
 		{
