c38b3cbc6fce19fd1a43722fe6de8dfd8f53548b
vickyrehinat <vickyrehinat@gmail.com>
2022-11-03 13:12:52 -0700

    task 1

---
+++ 0x07-pointers_arrays_strings/0-puts_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _puts_recursion - print a string
+ * @s: strings
+ * Retirn: void
+ */
+void _puts_recursion(char *s)
+{
+if (*s)
+{
+	_putchar(char *s)
+		_puts_recursion(s + 1);
+}
+else
+{
+	_putchar('\n');
+}
+
+}
+
