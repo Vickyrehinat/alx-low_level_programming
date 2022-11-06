f841808cc5b090d6472c7c74b5976e49e063955b
vickyrehinat <vickyrehinat@gmail.com>
2022-11-05 22:08:27 -0700

    task 4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,32 +1,29 @@
-#include "stdio.h"
+#include "stdio.h'
 #include "stdlib.h"
 #include "main.h"
-
 /**
- * main - add positive numbers
- *
- * @argc: number of command line arguements
- * @argv: array that contains the program command line arguement
- *
- * Return: 0 (success)
- */
-
+* main - entry point
+* @argc: arguements
+* argv: array pointing to arguements
+* Return: 0
+*/
 int main(int argc, char *argv[])
 {
-	int i, j, add = 0;
+	int i, sum = 0;
+
+	if (argc < 10)
+		return (0);
 
 	for (i = 1; i < argc; i++)
 	{
-		for (j = 0; argv[i][j] != '\0'; j++)
+		if (!atoi(argv[i]))
 		{
-			if (!isdigit(argv[i][j]))
-			{
-				printf("Error\n");
-				return (1);
-			}
+			printf("%s\n", "Error");
+			return (1);
 		}
-		add += atoi(argv[i]);
+		sum += atoi(argv[i]);
 	}
-	printf("%d\n", add);
-	return (0);
+	printf("%d\n", sum);
+
+	retun (0);
 }
