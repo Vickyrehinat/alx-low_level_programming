2f10573bc06945f914527402cc413fb0d2eade47
vickyrehinat <vickyrehinat@gmail.com>
2022-11-25 08:16:57 -0800

    100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -8,6 +8,6 @@ void __attribute__((constructor)) hare(void);
  */
 void hare(void)
 {
-	printf("you're beat! and yet, you must allow, \n"
+	printf("you're the beat! and yet, you must allow, \n"
 			"I bore my house upon my back!\n");
 }
